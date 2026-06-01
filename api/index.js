39c31ac2c86c3ee994700001b40a7e3f82c33ef7
pruthvi <pruthviraj162004@gmail.com>
2026-06-01 11:55:41 +0530

    create test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
-import express from 'express'
+import express, { json } from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js'
 dotenv.config();
 
 const app = express();
@@ -10,6 +11,9 @@ mongoose.connect(process.env.MONGO).then(() => {
 }).catch((err) => {
     console.log(err);
 })
+
 app.listen(3000, () => {
     console.log("app is listening on 3000");
 });
+
+app.use('/api/user',userRoutes)
