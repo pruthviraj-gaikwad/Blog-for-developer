86529b02c560dd41f242bf3684235dc7fd668f2b
pruthvi <pruthviraj162004@gmail.com>
2026-06-03 22:27:41 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -36,6 +37,40 @@ export const signup = async (req, res, next) => {
         return next(errorHandler(409, "Email already exists"));
       }
     }
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    if (!email || !password || email === "" || password === "") {
+      return next(errorHandler(400, "All Fields are required"));
+    }
+    const validUser = await User.findOne({ email: email.toLowerCase() });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, "Invalid password"));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+        sameSite: "strict",
+      })
+      .json({
+        _id: validUser._id,
+        username: validUser.username,
+        email: validUser.email,
+      });
+  } catch (error) {
     next(error);
   }
 };
